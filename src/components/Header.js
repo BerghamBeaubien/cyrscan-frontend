7002cc4f2221a12bfc47a5f58520c1a0e3b80e40
Mouad Khalladi <157147306+BerghamBeaubien@users.noreply.github.com>
2025-09-02 18:35:53 -0400

    ESLint fix for Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-﻿import React, { useContext } from 'react';
+import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 import { LogOut } from 'lucide-react';
